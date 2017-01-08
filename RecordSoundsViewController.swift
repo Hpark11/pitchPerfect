a54f567d5d4300a6da8fcbab5d3f302ab62367ad
HyunsooPark <qkrgustnrk@gmail.com>
2017-01-08 13:13:32 +0900

    Changed the name of initial VC

---
+++ RecordSoundsViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  pitchPerfect
+//
+//  Created by connect on 2017. 1. 8..
+//  Copyright © 2017년 connect. All rights reserved.
+//
+
+import UIKit
+
+class RecordSoundsViewController: UIViewController {
+
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
