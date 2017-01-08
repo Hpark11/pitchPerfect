c8ee3424b06640e03e757c0f27b510fb5c5f0b18
HyunsooPark <qkrgustnrk@gmail.com>
2017-01-08 15:21:56 +0900

    Enabling and disabling buttons
    Added images
    and now we can enable and disable record buttons
    navigation is also added

--- RecordSoundsViewController.swift
+++ RecordSoundsViewController.swift
@@ -11,20 +11,31 @@ import UIKit
 class RecordSoundsViewController: UIViewController {
 
     @IBOutlet weak var recordingLabel: UILabel!
+    @IBOutlet weak var recordButton: UIButton!
+    @IBOutlet weak var stopRecordingButton: UIButton!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        stopRecordingButton.isEnabled = false
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        print("viewWillappear")
     }
 
     // Start Recording...
-    @IBAction func recordAudio(_ sender: Any) { 
-        recordingLabel.text = "Recording In Progress "
+    @IBAction func recordAudio(_ sender: Any) {
+        recordingLabel.text = "Recording In Progress"
+        recordButton.isEnabled = false
+        stopRecordingButton.isEnabled = true
     }
     
     // Stop Recording and move on to next VC 
     @IBAction func stopRecording(_ sender: Any) {
-        print("aa")
+        recordingLabel.text = "Tab to Record"
+        recordButton.isEnabled = true
+        stopRecordingButton.isEnabled = false
     }
     
 
