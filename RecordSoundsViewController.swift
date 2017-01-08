70a6770a1d144bf958fdb2e836b8b930990818ee
HyunsooPark <qkrgustnrk@gmail.com>
2017-01-08 15:54:32 +0900

    AV audio session applied to record and stop recording button
    Recording file path checked

--- RecordSoundsViewController.swift
+++ RecordSoundsViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  RecordSoundsViewController.swift
 //  pitchPerfect
 //
 //  Created by connect on 2017. 1. 8..
@@ -7,9 +7,12 @@
 //
 
 import UIKit
+import AVFoundation
 
 class RecordSoundsViewController: UIViewController {
 
+    var audioRecorder: AVAudioRecorder!
+    
     @IBOutlet weak var recordingLabel: UILabel!
     @IBOutlet weak var recordButton: UIButton!
     @IBOutlet weak var stopRecordingButton: UIButton!
@@ -29,6 +32,20 @@ class RecordSoundsViewController: UIViewController {
         recordingLabel.text = "Recording In Progress"
         recordButton.isEnabled = false
         stopRecordingButton.isEnabled = true
+        
+        let dirPath = NSSearchPathForDirectoriesInDomains(.documentDirectory,.userDomainMask, true)[0] as String
+        let recordingName = "recordedVoice.wav"
+        let pathArray = [dirPath, recordingName]
+        let filePath = URL(string: pathArray.joined(separator: "/"))
+        print(filePath as Any)
+    
+        let session = AVAudioSession.sharedInstance()
+        try! session.setCategory(AVAudioSessionCategoryPlayAndRecord, with:AVAudioSessionCategoryOptions.defaultToSpeaker)
+        
+        try! audioRecorder = AVAudioRecorder(url: filePath!, settings: [:])
+        audioRecorder.isMeteringEnabled = true
+        audioRecorder.prepareToRecord()
+        audioRecorder.record()
     }
     
     // Stop Recording and move on to next VC 
@@ -36,6 +53,10 @@ class RecordSoundsViewController: UIViewController {
         recordingLabel.text = "Tab to Record"
         recordButton.isEnabled = true
         stopRecordingButton.isEnabled = false
+        
+        audioRecorder.stop()
+        let audioSession = AVAudioSession.sharedInstance()
+        try! audioSession.setActive(false)
     }
     
 
